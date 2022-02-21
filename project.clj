4e95fab2f84e6f099a6bc338d3604becded7625c
Amit Shrestha <amit.p.shrestha@gmail.com>
2022-02-21 12:43:29 -0800

    Bump up minor version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rorokimdim/pp-grid "0.1.9"
+(defproject org.clojars.rorokimdim/pp-grid "0.1.10"
   :description "A clojure library for constructing formatted text."
   :url "https://github.com/rorokimdim/pp-grid"
   :license {:name "MIT License"
